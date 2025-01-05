2fc5c95c22764dd458dbea7b48e9401d63596b1a
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2025-01-05 11:11:29 +0500

    changes

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { loginUsers, validateLoginUser } from "../mongoDB/models/login.js";
+import { validateLoginUser } from "../mongoDB/models/login.js";
 import { users } from "../mongoDB/models/register.js";
-import { status } from "init";
 
 export const loginUserRouter = express.Router();
 
@@ -16,29 +15,19 @@ loginUserRouter.post("/", async (req, res) => {
       });
     }
     const { email, password } = loginUserData;
-    const registerUsers = await users.find();
-    const user = registerUsers.find((user) => {
-      user.email === email && user.password === password;
-      return user;
-    });
+    const user = await users.findOne({ email, password });
     if (user) {
-      console.log("user mil gya =>", user);
+      console.log("🚀 ~ loginUserRouter.post ~ user:", user);
       return res.status(200).json({
         status: 200,
-        message: "logged in successfull",
+        message: "log in successfull",
         user: user,
       });
     }
-    if (!user.email)
-      return res.status(404).json({
-        status: 404,
-        message: "User not Found",
-      });
-    if (!user)
-      return res.status(401).json({
-        status: 401,
-        message: "Invalid cradientials",
-      });
+    return res.status(401).json({
+      status: 401,
+      message: "Invalid credientials",
+    });
   } catch (error) {
     console.log("🚀 ~ loginUserRouter.post ~ error:", error);
     return res.status(500).json({
